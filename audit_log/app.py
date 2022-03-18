5d43aae9e3d24abc57c44edbf0dc77a17d426761
shahinysf <shahin.ysf@gmail.com>
2022-03-18 12:56:12 -0700

    Update app.py

--- audit_log/app.py
+++ audit_log/app.py
@@ -1,1 +1,86 @@
 
+import connexion
+import yaml
+import logging.config
+import datetime
+import json
+from pykafka import KafkaClient
+from pykafka.common import OffsetType
+from threading import Thread
+
+with open('app_conf.yml', 'r') as f:
+    app_config = yaml.safe_load(f.read())
+
+with open('log_conf.yml', 'r') as f:
+    log_config = yaml.safe_load(f.read())
+    logging.config.dictConfig(log_config)
+
+logger = logging.getLogger('basicLogger')
+
+
+def get_ride_request_reading(index):
+    """ Get RR Reading in History """
+    hostname = "%s:%d" % (app_config["events"]["hostname"],
+                          app_config["events"]["port"])
+    client = KafkaClient(hosts=hostname)
+    topic = client.topics[str.encode(app_config["events"]["topic"])]
+
+    consumer = topic.get_simple_consumer(reset_offset_on_start=True,
+                                         consumer_timeout_ms=1000)
+
+    logger.info("Retrieving RR at index %d" % index)
+
+    try:
+        i = 0
+        for msg in consumer:
+            msg_str = msg.value.decode('utf-8')
+            msg = json.loads(msg_str)
+            if msg["type"] == "eventride" and i == index:
+                return msg["payload"], 200
+            elif i == index:
+                return {"message": "Not Found"}, 404
+            i += 1
+
+    except:
+        logger.error("No more messages found")
+
+    logger.error("Could not find RR at index %d" % index)
+    return {"message": "Not Found"}, 404
+
+
+def get_schedule_request_reading(index):
+    """ Get RR Reading in History """
+    hostname = "%s:%d" % (app_config["events"]["hostname"],
+                          app_config["events"]["port"])
+    client = KafkaClient(hosts=hostname)
+    topic = client.topics[str.encode(app_config["events"]["topic"])]
+
+    consumer = topic.get_simple_consumer(reset_offset_on_start=True,
+                                         consumer_timeout_ms=1000)
+
+    logger.info("Retrieving SR at index %d" % index)
+
+    try:
+        i = 0
+        for msg in consumer:
+            msg_str = msg.value.decode('utf-8')
+            msg = json.loads(msg_str)
+            if msg["type"] == "eventschedule" and i == index:
+                return msg["payload"], 200
+            elif i == index:
+                return {"message": "Not Found"}, 404
+
+            i += 1
+
+    except:
+        logger.error("No more messages found")
+
+    logger.error("Could not find RR at index %d" % index)
+    return {"message": "Not Found"}, 404
+
+
+app = connexion.FlaskApp(__name__, specification_dir='')
+app.add_api("./openapi.yml", strict_validation=True, validate_responses=True)
+
+if __name__ == "__main__":
+    app.run(port=8110, debug=True)

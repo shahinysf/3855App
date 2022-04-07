aae47d659f3f75d3f9938a190a49d2d10a622b4f
shahinysf <shahin.ysf@gmail.com>
2022-04-07 14:35:37 -0700

    Update app.py

--- audit_log/app.py
+++ audit_log/app.py
@@ -8,6 +8,16 @@ from pykafka import KafkaClient
 from pykafka.common import OffsetType
 from threading import Thread
 from flask_cors import CORS, cross_origin
+import os
+
+if "TARGET_ENV" in os.environ and os.environ["TARGET_ENV"] == "test":
+    print("In Test Environment")
+    app_conf_file = "/config/app_conf.yml"
+    log_conf_file = "/config/log_conf.yml"
+else:
+    print("In Dev Environment")
+    app_conf_file = "app_conf.yml"
+    log_conf_file = "log_conf.yml"
 
 with open('app_conf.yml', 'r') as f:
     app_config = yaml.safe_load(f.read())
@@ -17,6 +27,8 @@ with open('log_conf.yml', 'r') as f:
     logging.config.dictConfig(log_config)
 
 logger = logging.getLogger('basicLogger')
+logger.info("App Conf File: %s" % app_conf_file) 
+logger.info("Log Conf File: %s" % log_conf_file)
 
 
 def get_ride_request_reading(index):

fde1dad457203288e8fd6295114dbac6b957b645
shahinysf <shahin.ysf@gmail.com>
2022-04-07 14:31:33 -0700

    Update app.py

--- receiver/app.py
+++ receiver/app.py
@@ -9,15 +9,28 @@ import uuid
 import datetime
 from pykafka import KafkaClient
 import time
+import os
+
+if "TARGET_ENV" in os.environ and os.environ["TARGET_ENV"] == "test":
+    print("In Test Environment")
+    app_conf_file = "/config/app_conf.yml"
+    log_conf_file = "/config/log_conf.yml"
+else:
+    print("In Dev Environment")
+    app_conf_file = "app_conf.yml"
+    log_conf_file = "log_conf.yml"
 
 with open('app_conf.yml', 'r') as f:
     app_config = yaml.safe_load(f.read())
 
+# External Logging Configuration
 with open('log_conf.yml', 'r') as f:
     log_config = yaml.safe_load(f.read())
     logging.config.dictConfig(log_config)
 
 logger = logging.getLogger('basicLogger')
+logger.info("App Conf File: %s" % app_conf_file) 
+logger.info("Log Conf File: %s" % log_conf_file)
 
 max_retry = app_config['connection']['maxretry']
 current_retry = 0
